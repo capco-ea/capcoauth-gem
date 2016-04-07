1264137bcafdfd9c2e29fe18da14bcae8e64732d
Adam Robertson <adam@arcreative.net>
2016-04-07 12:28:44 -0700

    Uncomment client_id and client_secret so it fails more obviously in the browser

--- lib/generators/capcoauth/templates/initializer.rb
+++ lib/generators/capcoauth/templates/initializer.rb
@@ -1,9 +1,9 @@
 Capcoauth.configure do |config|
   # CapcOAuth Client ID
-  # config.client_id 'YOUR APPLICATION CLIENT ID'
+  config.client_id 'YOUR APPLICATION CLIENT ID'
 
   # CapcOAuth Client Secret
-  # config.client_secret 'YOUR APPLICATION CLIENT SECRET'
+  config.client_secret 'YOUR APPLICATION CLIENT SECRET'
 
   # Configures how often to check CapcOAuth for access token validity, in seconds.  If this value is too high,
   # application will continue to serve requests to users even after the token is revoked
