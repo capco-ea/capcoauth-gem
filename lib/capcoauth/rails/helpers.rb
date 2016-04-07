f5d168683dd19632295bf381453fa734f4dd5388
Adam Robertson <adam@arcreative.net>
2016-04-07 11:36:13 -0700

    Failed auth check should delete session vars to be safe

--- lib/capcoauth/rails/helpers.rb
+++ lib/capcoauth/rails/helpers.rb
@@ -7,6 +7,8 @@ module Capcoauth
         if capcoauth_token
           session.delete(:previous_url)
         else
+          session.delete(:capcoauth_access_token)
+          session.delete(:capcoauth_user_id)
           session[:previous_url] = request.url
           redirect_to :auth_login
         end
