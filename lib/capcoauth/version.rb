0fadf1cfc1a97d674f804cb5c9641117cc8f5153
Adam Robertson <adam@arcreative.net>
2023-10-02 19:37:37 -0700

    v1.0.0

--- lib/capcoauth/version.rb
+++ lib/capcoauth/version.rb
@@ -2,10 +2,9 @@ module Capcoauth
   def self.gem_version
     Gem::Version.new VERSION::STRING
   end
-
   module VERSION
-    MAJOR = 0
-    MINOR = 11
+    MAJOR = 1
+    MINOR = 0
     PATCH  = 0
 
     STRING = [MAJOR, MINOR, PATCH].compact.join(".")
